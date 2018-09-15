ec89242092bde1fad9cb5acb6095726bfc3047db
Maxim Shatalov <shatalov04@gmail.com>
2018-09-15 22:21:15 +0300

    add point and map tests

---
+++ TreehouseDefenseTests/MapTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TreehouseDefense;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreehouseDefense.Tests
+{
+    [TestClass()]
+    public class MapTests
+    {
+        [TestMethod()]
+        public void MapTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void OnMapTest()
+        {
+
+            var x = 10;
+            var y = 12;
+            var width = 13;
+            var height = 14;
+
+            var point = new Point(x, y);
+            var map = new Map(width, height);
+
+            Assert.IsTrue(map.OnMap(point), "Point should be on map");
+
+        }
+    }
+}
---
+++ TreehouseDefenseTests/PointTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TreehouseDefense;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreehouseDefense.Tests
+{
+    [TestClass()]
+    public class PointTests
+    {
+        [TestMethod()]
+        public void PointTest()
+        {
+            var x = 5;
+            var y = 6;
+
+            var point = new Point(x,y);
+
+            Assert.AreEqual(x,point.X);
+            Assert.AreEqual(y,point.Y);
+        }
+
+        [TestMethod()]
+        public void DistanceToTest()
+        {
+           Assert.Fail();
+        }
+    }
+}
