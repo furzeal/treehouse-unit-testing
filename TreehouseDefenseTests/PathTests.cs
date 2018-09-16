0fd2fb56c71ce58fcd15b2b6bc50b8ce08fbf3fa
Maxim Shatalov <shatalov04@gmail.com>
2018-09-16 18:29:12 +0300

    add tdd tests for path class

---
+++ TreehouseDefenseTests/PathTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TreehouseDefense.Tests
+{
+    [TestClass()]
+    public class PathTests
+    {
+        private Map _map3x3;
+        private MapLocation[] _pathLocations3;
+        private Path _path3;
+
+        [TestCleanup]
+        public void testClean()
+        {
+            _map3x3 = null;
+            _pathLocations3 = null;
+            _path3 = null;
+        }
+
+        [TestInitialize]
+        public void testInit()
+        {
+            _map3x3 = new Map(3, 3);
+            _pathLocations3 = new MapLocation[]
+            {
+                new MapLocation(0, 1, _map3x3),
+                new MapLocation(1, 1, _map3x3),
+                new MapLocation(2, 1, _map3x3),
+            };
+
+            _path3 = new Path(_pathLocations3);
+        }
+
+        [TestMethod]
+        public void MapLocationIsOnPath()
+        {
+            
+            var target = _path3;
+
+            Assert.IsTrue(target.IsOnPath(new MapLocation(0, 1, _map3x3)));
+        }
+
+        [TestMethod]
+        public void MapLocationIsNotOnPath()
+        {
+            var target = _path3;
+
+            Assert.IsFalse(target.IsOnPath(new MapLocation(0,0, _map3x3)));
+        }
+    }
+}
